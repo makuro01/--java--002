183fa31c57c710f30cf82f05cff6bbcb01474c96
zxl <xiaoarashi@gmail.com>
2024-08-28 20:39:55 +0900

    controller goods commit

---
+++ goods/src/main/java/com/zxlstu/sns/GoodsController.java
@@ -0,0 +1,11 @@
+package com.zxlstu.sns;
+
+/**
+ * @author 張　曉竜
+ * @java se version 1.0
+ **/
+public class GoodsController {
+    public static void main(String[] args) {
+        System.out.println("controller");
+    }
+}
