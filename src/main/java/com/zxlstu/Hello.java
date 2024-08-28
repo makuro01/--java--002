0c4a1edaa87fb6274a2e9ee5151fc639916338e8
zxl <xiaoarashi@gmail.com>
2024-08-28 17:38:14 +0900

    Hello word commit no2

--- src/main/java/com/zxlstu/Hello.java
+++ src/main/java/com/zxlstu/Hello.java
@@ -7,5 +7,6 @@ package com.zxlstu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello zxl");
     }
 }
